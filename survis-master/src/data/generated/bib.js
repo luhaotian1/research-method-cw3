24ba444d500991318facb07d4b7a3dd399586115
luhaotian1 <62682499+luhaotian1@users.noreply.github.com>
2024-05-16 21:48:27 +0100

    Add files via upload

--- survis-master/src/data/generated/bib.js
+++ survis-master/src/data/generated/bib.js
@@ -1,10 +1,12 @@
 ﻿const generatedBibEntries = {
     "alsaigh2023ai": {
         "author": "Alsaigh, Roba and Mehmood, Rashid and Katib, Iyad",
+        "doi": "10.1016/j.inffus.2019.12.012",
         "journal": "Frontiers in Energy Research",
+        "keywords": "type:survey report",
         "pages": "1071291",
         "publisher": "Frontiers Media SA",
-        "title": "AI explainability and governance in smart energy systems: a review",
+        "title": "{AI} explainability and governance in smart energy systems: a review",
         "type": "article",
         "volume": "11",
         "year": "2023"
@@ -12,6 +14,7 @@
     "arrieta2020explainable": {
         "author": "Arrieta, Alejandro Barredo and D{\\'\\i}az-Rodr{\\'\\i}guez, Natalia and Del Ser, Javier and Bennetot, Adrien and Tabik, Siham and Barbado, Alberto and Garc{\\'\\i}a, Salvador and Gil-L{\\'o}pez, Sergio and Molina, Daniel and Benjamins, Richard and others",
         "journal": "Information fusion",
+        "keywords": "type:survey report",
         "pages": "82--115",
         "publisher": "Elsevier",
         "title": "Explainable Artificial Intelligence (XAI): Concepts, taxonomies, opportunities and challenges toward responsible AI",
@@ -22,6 +25,7 @@
     "aslam2022anomaly": {
         "author": "Aslam, Nida and Khan, Irfan Ullah and Alansari, Aisha and Alrammah, Marah and Alghwairy, Atheer and Alqahtani, Rahaf and Alqahtani, Razan and Almushikes, Maryam and Hashim, Mohammed AL and others",
         "journal": "Applied Computational Intelligence and Soft Computing",
+        "keywords": "type:industry application",
         "publisher": "Hindawi",
         "title": "Anomaly detection using explainable random forest for the prediction of undesirable events in oil wells",
         "type": "article",
@@ -31,6 +35,7 @@
     "blanco2019machine": {
         "author": "Blanco-Justicia, Alberto and Domingo-Ferrer, Josep",
         "booktitle": "Machine Learning and Knowledge Extraction: Third IFIP TC 5, TC 12, WG 8.4, WG 8.9, WG 12.9 International Cross-Domain Conference, CD-MAKE 2019, Canterbury, UK, August 26--29, 2019, Proceedings 3",
+        "keywords": "type:method reaserch",
         "organization": "Springer",
         "pages": "15--26",
         "title": "Machine learning explainability through comprehensible decision trees",
@@ -40,6 +45,7 @@
     "fernandez2020random": {
         "author": "Fern{\\'a}ndez, Rub{\\'e}n R and De Diego, Isaac Mart{\\'\\i}n and Ace{\\~n}a, V{\\'\\i}ctor and Fern{\\'a}ndez-Isabel, Alberto and Moguerza, Javier M",
         "journal": "Information Fusion",
+        "keywords": "type:method reaserch",
         "pages": "196--207",
         "publisher": "Elsevier",
         "title": "Random forest explainability using counterfactual sets",
@@ -50,6 +56,7 @@
     "mohseni2021multidisciplinary": {
         "author": "Mohseni, Sina and Zarei, Niloofar and Ragan, Eric D",
         "journal": "ACM Transactions on Interactive Intelligent Systems (TiiS)",
+        "keywords": "type:survey report",
         "number": "3-4",
         "pages": "1--45",
         "publisher": "ACM New York, NY",
@@ -61,6 +68,7 @@
     "naser2021engineer": {
         "author": "Naser, MZ",
         "journal": "Automation in Construction",
+        "keywords": "type:industry application",
         "pages": "103821",
         "publisher": "Elsevier",
         "title": "An engineer's guide to eXplainable Artificial Intelligence and Interpretable Machine Learning: Navigating causality, forced goodness, and the false perception of inference",
@@ -71,6 +79,7 @@
     "petkovic2018improving": {
         "author": "Petkovic, Dragutin and Altman, Russ and Wong, Mike and Vigil, Arthur",
         "booktitle": "Pacific symposium on biocomputing 2018: proceedings of the pacific symposium",
+        "keywords": "type:method reaserch",
         "organization": "World Scientific",
         "pages": "204--215",
         "title": "Improving the explainability of Random Forest classifier--user centered approach",
@@ -80,6 +89,7 @@
     "preece2018asking": {
         "author": "Preece, Alun",
         "journal": "Intelligent Systems in Accounting, Finance and Management",
+        "keywords": "type:survey report",
         "number": "2",
         "pages": "63--72",
         "publisher": "Wiley Online Library",
@@ -91,6 +101,7 @@
     "sagi2020explainable": {
         "author": "Sagi, Omer and Rokach, Lior",
         "journal": "Information Fusion",
+        "keywords": "type:method reaserch",
         "pages": "124--138",
         "publisher": "Elsevier",
         "title": "Explainable decision forest: Transforming a decision forest into an interpretable tree",
